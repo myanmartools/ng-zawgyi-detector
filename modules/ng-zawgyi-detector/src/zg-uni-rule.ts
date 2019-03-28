6b9dd7c1bca5b6274926da29e663c22098f88d69
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-28 16:00:18 +0630

    Initial commit

---
+++ modules/ng-zawgyi-detector/src/zg-uni-rule.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export interface ZgUniRuleItem {
+    test: string;
+    minInputLength: number;
+    maxInputLength?: number;
+    excludes?: string[] | null;
+    description?: string;
+}
+
+export interface ZgUniRule {
+    zgRules: ZgUniRuleItem[];
+    uniRules: ZgUniRuleItem[];
+}
