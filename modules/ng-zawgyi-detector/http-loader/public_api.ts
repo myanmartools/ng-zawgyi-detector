7eece55242f3ad3fbc261abfcf48e1d6cdfac1cc
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-28 15:58:58 +0630

    Initial commit

---
+++ modules/ng-zawgyi-detector/http-loader/public_api.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export { HTTP_ZG_UNI_RULE_LOADER_OPTIONS, HttpZgUniRuleLoader, HttpZgUniRuleLoaderOptions } from './src/http-zg-uni-rule-loader';
+export { HttpZgUniRuleLoaderModule } from './src/http-zg-uni-rule-loader.module';
