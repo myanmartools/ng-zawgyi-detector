efa97f1d6e6aaf7b4a2d6dfdea6f3ec7012b23c2
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-28 15:59:44 +0630

    Initial commit

---
+++ modules/ng-zawgyi-detector/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export * from './noop-zg-uni-rule-loader.module';
+export * from './zawgyi-detector.module';
+export * from './zawgyi-detector';
+export * from './zg-uni-detect-result';
+export * from './zg-uni-rule-loader.token';
+export * from './zg-uni-rule-loader';
+export * from './zg-uni-rule-store';
+export * from './zg-uni-rule';
