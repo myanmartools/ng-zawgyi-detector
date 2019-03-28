5c2bccddf6ac28057f4ce0f4e5946c09d35cf03a
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-28 15:59:33 +0630

    Initial commit

---
+++ modules/ng-zawgyi-detector/http-loader/src/http-zg-uni-rule-loader.module.ts
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+// tslint:disable: no-unnecessary-class
+
+import { ModuleWithProviders, NgModule } from '@angular/core';
+
+import { ZG_UNI_RULE_LOADER } from '@myanmartools/ng-zawgyi-detector';
+
+import { HTTP_ZG_UNI_RULE_LOADER_OPTIONS, HttpZgUniRuleLoader, HttpZgUniRuleLoaderOptions } from './http-zg-uni-rule-loader';
+
+@NgModule({
+    providers: [
+        {
+            provide: ZG_UNI_RULE_LOADER,
+            useClass: HttpZgUniRuleLoader
+        }
+    ]
+})
+export class HttpZgUniRuleLoaderModule {
+    static withOptions(options: HttpZgUniRuleLoaderOptions = {}): ModuleWithProviders<HttpZgUniRuleLoaderModule> {
+        return {
+            ngModule: HttpZgUniRuleLoaderModule,
+            providers: [
+                {
+                    provide: HTTP_ZG_UNI_RULE_LOADER_OPTIONS,
+                    useValue: options
+                }
+            ],
+        };
+    }
+}
