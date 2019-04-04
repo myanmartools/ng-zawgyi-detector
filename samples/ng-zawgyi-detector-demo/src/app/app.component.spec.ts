80a66febe94d9e9f47f4f9358e2e1730b66b764f
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-05 06:20:52 +0630

    Rename to ng-zawgyi-detector-demo

---
+++ samples/ng-zawgyi-detector-demo/src/app/app.component.spec.ts
@@ -0,0 +1,49 @@
+// tslint:disable: no-floating-promises
+
+import { async, TestBed } from '@angular/core/testing';
+
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule, MatToolbarModule } from '@angular/material';
+
+// tslint:disable: no-implicit-dependencies
+import { NoopZgUniRuleLoaderModule, ZawgyiDetectorModule } from '@myanmartools/ng-zawgyi-detector';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                NoopAnimationsModule,
+                CommonModule,
+
+                FlexLayoutModule,
+                MatInputModule,
+                MatToolbarModule,
+
+                ZawgyiDetectorModule,
+                NoopZgUniRuleLoaderModule
+            ],
+        }).compileComponents();
+    }));
+
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+    it('should render title in a h1 tag', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement as HTMLElement;
+        const ele = compiled.querySelector('h1');
+        expect(ele && ele.textContent).toContain('Zawgyi-One or Myanmar Unicode Font Detector Demo');
+    });
+});
