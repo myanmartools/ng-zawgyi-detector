e087d028249007562f8aa11096549d6e592e437d
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-05 07:28:28 +0630

    Update app title

--- samples/ng-zawgyi-detector-demo/e2e/src/app.e2e-spec.ts
+++ samples/ng-zawgyi-detector-demo/e2e/src/app.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('workspace-project App', () => {
         page.navigateTo();
 
         // tslint:disable-next-line: no-floating-promises
-        expect(page.getTitleText()).toEqual('ng-zawgyi-detector-demo');
+        expect(page.getTitleText()).toEqual('ng-zawgyi-detector demo');
     });
 
     afterEach(async () => {
