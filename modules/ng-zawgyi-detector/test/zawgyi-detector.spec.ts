bf883f7a14ae14f2f3893247ec8309bd2068802c
Moe Myat Zaw <mmzliveid@gmail.com>
2019-08-26 17:10:04 +0630

    Update tests

--- modules/ng-zawgyi-detector/test/zawgyi-detector.spec.ts
+++ modules/ng-zawgyi-detector/test/zawgyi-detector.spec.ts
@@ -83,20 +83,20 @@ describe('ZawgyiDetector#detect', () => {
 
     // Mix detection
     // ----------------------------------------------------------------------
-    it(String.raw`should detect input '\u103B\u1000\n\u1000\u1031' as 'zg'`, () => {
-        const result = zawgyiDetector.detect('\u103B\u1000\n\u1000\u1031');
+    it(String.raw`should detect input '\u103B\u1000(Unicode)\n\u1000\u1031' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u103B\u1000(Unicode)\n\u1000\u1031');
         expect(result.detectedEnc).toBe('mix');
         expect(result.matches.length).toBe(2);
 
         expect(result.matches[0].detectedEnc).toBe('zg');
         expect(result.matches[0].probability).toBeGreaterThanOrEqual(0.5);
         expect(result.matches[0].start).toBe(0);
-        expect(result.matches[0].length).toBe(3);
-        expect(result.matches[0].matchedString).toBe('\u103B\u1000\n');
+        expect(result.matches[0].length).toBe(12);
+        expect(result.matches[0].matchedString).toBe('\u103B\u1000(Unicode)\n');
 
         expect(result.matches[1].detectedEnc).toBe('uni');
         expect(result.matches[1].probability).toBeGreaterThanOrEqual(0.5);
-        expect(result.matches[1].start).toBe(3);
+        expect(result.matches[1].start).toBe(12);
         expect(result.matches[1].length).toBe(2);
         expect(result.matches[1].matchedString).toBe('\u1000\u1031');
     });
