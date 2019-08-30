505d8c83eb9407c2fdd3c1043cf0e033d3202a90
Moe Myat Zaw <mmzliveid@gmail.com>
2019-08-30 13:18:12 +0630

    Update tests

--- modules/ng-zawgyi-detector/test/zawgyi-detector-zgc.spec.ts
+++ modules/ng-zawgyi-detector/test/zawgyi-detector-zgc.spec.ts
@@ -17,6 +17,8 @@ describe('ZawgyiDetector#detect#zgc', () => {
         zawgyiDetector = TestBed.get<ZawgyiDetector>(ZawgyiDetector) as ZawgyiDetector;
     });
 
+    // Possible Zg Chars
+    //
     it(String.raw`should detect input '\u104E' as 'zg'`, () => {
         const result = zawgyiDetector.detect('\u104E');
         expect(result.detectedEnc).toBe('zg');
@@ -25,7 +27,6 @@ describe('ZawgyiDetector#detect#zgc', () => {
         expect(result.matches[0].probability).toBe(1);
         expect(result.matches[0].start).toBe(0);
         expect(result.matches[0].length).toBe(1);
-        expect(result.matches[0].matchedString).toBe('\u104E');
     });
 
     it(String.raw`should detect input '\u104E\u106A\u106B\u1086\u108F\u1090' as 'zg'`, () => {
@@ -36,7 +37,6 @@ describe('ZawgyiDetector#detect#zgc', () => {
         expect(result.matches[0].probability).toBe(1);
         expect(result.matches[0].start).toBe(0);
         expect(result.matches[0].length).toBe(6);
-        expect(result.matches[0].matchedString).toBe('\u104E\u106A\u106B\u1086\u108F\u1090');
     });
 
     it(String.raw`should detect input '\n \u106A \u106B\u104B' as 'zg'`, () => {
@@ -47,7 +47,6 @@ describe('ZawgyiDetector#detect#zgc', () => {
         expect(result.matches[0].probability).toBe(1);
         expect(result.matches[0].start).toBe(0);
         expect(result.matches[0].length).toBe(6);
-        expect(result.matches[0].matchedString).toBe('\n \u106A \u106B\u104B');
     });
 
     it(String.raw`should detect input '\u1000\u104E' as 'zg'`, () => {
@@ -58,7 +57,6 @@ describe('ZawgyiDetector#detect#zgc', () => {
         expect(result.matches[0].probability).toBe(0.75);
         expect(result.matches[0].start).toBe(0);
         expect(result.matches[0].length).toBe(2);
-        expect(result.matches[0].matchedString).toBe('\u1000\u104E');
     });
 
     it(String.raw`should detect input '\u104E\u1000' as 'zg'`, () => {
@@ -69,17 +67,59 @@ describe('ZawgyiDetector#detect#zgc', () => {
         expect(result.matches[0].probability).toBe(0.76);
         expect(result.matches[0].start).toBe(0);
         expect(result.matches[0].length).toBe(2);
-        expect(result.matches[0].matchedString).toBe('\u104E\u1000');
     });
 
-    // it(String.raw`should detect input '\u1025\u1033' as 'zg'`, () => {
-    //     const result = zawgyiDetector.detect('\u1025\u1033');
-    //     expect(result.detectedEnc).toBe('zg');
-    //     expect(result.matches.length).toBe(1);
-    //     expect(result.matches[0].probability).toBeGreaterThanOrEqual(0.5);
-    //     expect(result.matches[0].start).toBe(0);
-    //     expect(result.matches[0].length).toBe(2);
-    //     expect(result.matches[0].matchedString).toBe('\u1025\u1033');
-    // });
+    // \u1039
+    //
+    it(String.raw`should detect input '\u1000\u1039' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u1000\u1039');
+        expect(result.detectedEnc).toBe('zg');
+        expect(result.matches.length).toBe(1);
+        expect(result.matches[0].detectedEnc).toBe('zg');
+        expect(result.matches[0].probability).toBe(0.54);
+        expect(result.matches[0].start).toBe(0);
+        expect(result.matches[0].length).toBe(2);
+    });
 
+    it(String.raw`should detect input '\u1000\u1039\u102F' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u1000\u1039\u102F');
+        expect(result.detectedEnc).toBe('zg');
+        expect(result.matches.length).toBe(1);
+        expect(result.matches[0].detectedEnc).toBe('zg');
+        expect(result.matches[0].probability).toBe(0.54);
+        expect(result.matches[0].start).toBe(0);
+        expect(result.matches[0].length).toBe(3);
+    });
+
+    it(String.raw`should detect input '\u1001\u1039\u102F' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u1001\u1039\u102F');
+        expect(result.detectedEnc).toBe('zg');
+        expect(result.matches.length).toBe(1);
+        expect(result.matches[0].detectedEnc).toBe('zg');
+        expect(result.matches[0].probability).toBe(0.54);
+        expect(result.matches[0].start).toBe(0);
+        expect(result.matches[0].length).toBe(3);
+    });
+
+    it(String.raw`should detect input '\u1000\u1000\u1039\u102F' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u1000\u1000\u1039\u102F');
+        expect(result.detectedEnc).toBe('zg');
+        expect(result.matches.length).toBe(1);
+        expect(result.matches[0].detectedEnc).toBe('zg');
+        expect(result.matches[0].probability).toBe(0.52);
+        expect(result.matches[0].start).toBe(0);
+        expect(result.matches[0].length).toBe(4);
+    });
+
+    // Possible Zg Ac
+    //
+    it(String.raw`should detect input '\u1025\u1033' as 'zg'`, () => {
+        const result = zawgyiDetector.detect('\u1025\u1033');
+        expect(result.detectedEnc).toBe('zg');
+        expect(result.matches.length).toBe(1);
+        expect(result.matches[0].detectedEnc).toBe('zg');
+        expect(result.matches[0].probability).toBe(1);
+        expect(result.matches[0].start).toBe(0);
+        expect(result.matches[0].length).toBe(2);
+    });
 });
